5a31d004651bbc1cfeff03f3a0a7a16e76fb0cc5
AndrewTheMaster <andreykabaryaev228@gmail.com>
2024-02-27 00:59:49 +0300

    working heikin ashi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
+import csv
 
 def getCandles(symb, tf, limit):
     url = 'https://api.bybit.com'
@@ -35,19 +36,61 @@ def getCandlesHeikenAshi(symb, tf, limit):
     df = pd.DataFrame(r.json()['result']['list'])
     print("data")
     print(df)
+    file_path = "CandlesHeikenAshi.csv"
 
-    # Преобразование в хайкен-аши
-    m = pd.DataFrame()
-    m['Date'] = df.iloc[:, 0].astype(np.int64)
-    m['Date'] = pd.to_datetime(m['Date'], unit='ms')
-    m['Close'] = (df.iloc[:, 1].astype(float) + df.iloc[:, 2].astype(float) + df.iloc[:, 3].astype(float) + df.iloc[:, 4].astype(float)) / 4.0
-    m['Open'] = (m['Close'].shift(1) + m['Close'].shift(1) + m['Close'].shift(1) + m['Close'].shift(1)) / 4.0
-    m['High'] = df.iloc[:, 1:5].max(axis=1)
-    m['Low'] = df.iloc[:, 1:5].min(axis=1)
-    m['Volume'] = df.iloc[:, 5].astype(float)
-    m = m.sort_values(by='Date')
+    with open(file_path, 'r', newline='', encoding='utf-8') as file:
+        CSVdf = pd.read_csv(file_path, encoding='utf-8')
+        print("last_row")
+        last_row = CSVdf.tail(1)
+        print(last_row)
+        #print(last_row.loc[291, 'Open'])
+        print("last_row")
+
+        # Преобразование в хайкен-аши
+        m = pd.DataFrame()
+        m['Date'] = df.iloc[:, 0].astype(np.int64)
+        m['Date'] = pd.to_datetime(m['Date'], unit='ms')
+        m['Close'] = (df.iloc[:, 1].astype(float) + df.iloc[:, 2].astype(float) + df.iloc[:, 3].astype(float) + df.iloc[:, 4].astype(float)) / 4.0
+        m['Open'] = (m['Close'].shift(1) + m['Close'].shift(1) + m['Close'].shift(1) + m['Close'].shift(1)) / 4.0
+        m['High'] = df.iloc[:, 2].astype(float)
+        m['Low'] = df.iloc[:, 3].astype(float)
+        m['Volume'] = df.iloc[:, 5].astype(float)
+        m = m.sort_values(by='Date')
+
+        # Найдем дату в DataFrame m, соответствующую дате в последней строке CSVdf
+        start_date = last_row['Date'].iloc[0]
+        print(start_date)
+
+        important_index =  last_row.index
+        print(important_index)
+        start_index = m[m['Date'] == pd.to_datetime(start_date)].index[0]
+        print(start_index)
+        # Начинаем с найденного индекса в DataFrame m
+        for index, row in m.iloc[limit-start_index:].iterrows():
+            # ваш код обработки каждой строки
+            print(index)
+            if index+1 == start_index:
+                m.loc[index, 'Open'] = (last_row['Open'].iloc[0] + last_row['Close'].iloc[0]) / 2.0
+
+                print((last_row['Open'].iloc[0] + last_row['Close'].iloc[0]) / 2.0)
+            else:
+                m.loc[index, 'Open'] = (m.loc[index + 1, 'Open'] + m.loc[index + 1, 'Close']) / 2.0
+
+            print(m.loc[index, 'Open'])
+        m['High'] = m.iloc[:, 1:5].max(axis=1)
+        m['Low'] = m.iloc[:, 1:5].min(axis=1)
+
+        m_without_last_row = m.iloc[:-1]  # Исключаем последнюю строку
+        m_without_last_row.to_csv("CandlesHeikenAshi.csv", index=False, mode='w')
+
 
     return m
+
+    
+    
+
+
+ 
    
 def getAlert(df, tf, OBMitigationType, sens):
     df['Date'] = pd.to_datetime(df['Date'])
